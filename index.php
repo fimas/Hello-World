ea2f41961a240e0448044ab8dade2e62d119ea37
Tomas Landberg <tomas@froghouse.se>
2011-07-14 00:09:46 +0200

    Signed-off-by: Tomas Landberg <tomas@froghouse.se>

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
     // Prints the string "Hello World!" to the screen
-    echo "Hello World!<br>\n";
+    class Car {
+        private $speed;
+        
+        public function __construct($s) {
+            $this->speed = $s; // Set initial speed
+        }
+        
+        public function accelerate() {
+            $this->speed++;
+        }
+        
+        public function decelerate() {
+            $this->speed--;
+        }
+        
+        public function getSpeed() {
+            return $this->speed;
+        }
+    }
+    
+    $car = new Car(25);
+    
+    echo $car->getSpeed(); // Prints 25
+    
+    $car->accelerate();
+    $car->decelerate();
+    $car->decelerate();
+    
+    echo $car->getSpeed(); // Prints 24
 ?>
